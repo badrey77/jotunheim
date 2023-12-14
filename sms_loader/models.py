9f1e06f5ed94e3cdfdfc19c1c2af6411c3f9dbab
badrey77 <badrey77@gmail.com>
2023-12-14 16:52:49 +0100

    Add Plantuml

--- sms_loader/models.py
+++ sms_loader/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import CharField, ForeignKey, BooleanField, SET_NULL
 
+<<<<<<< HEAD
+=======
+from sms_loader.variables import STATUS_MESSAGE
+>>>>>>> 2dd6654 (Add Plantuml)
 
 
 class Sms(models.Model):
@@ -9,7 +13,11 @@ class Sms(models.Model):
     destinatiare = CharField(max_length=255, verbose_name='destinataire')
     proprietaire = ForeignKey(get_user_model(), verbose_name='propriétaire', on_delete=SET_NULL)
     contenu = CharField(max_length=1000)
+<<<<<<< HEAD
     etat = CharField(max_length=2, choices=STATUS_MESSAGE)
+=======
+    status = CharField(max_length=2, choices=STATUS_MESSAGE, verbose_name='status')
+>>>>>>> 2dd6654 (Add Plantuml)
 
     def __str__(self):
         if self.pk == None :
